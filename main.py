ce06e6504d1f4db0df52ee45a3703d38b5f2f6da
Luis Mhaske <luis.mhaske007@outlook.com>
2023-09-24 20:43:54 +0100

    Initial commit <planning>.

---
+++ main.py
@@ -0,0 +1,25 @@
+"""
+Task to create a program to manage accounting system for a warehouse.
+
+Command and actions:
+Balance -> Add or Subtract balance.
+Sale -> should be able to sale a product i.e. if there is multiple product only one item should be sold
+        and balance (+=) and quantity updated.
+Purchase-> should add the item to the self and balance (-=) and quantity update(if item is already)
+account -> display current balance.
+list -> display total inventory
+warehouse -> Search for product and display its status.
+Review ->
+end ->
+
+"""
+COMMAND_LIST = """List of commands
+  - balance
+  - sale
+  - purchase
+  - account
+  - list
+  - warehouse
+  - review
+  - end
+"""
