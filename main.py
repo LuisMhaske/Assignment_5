2a7d4c18cbef4f9af2002c206ca3a5c52fe21b52
Luis Mhaske <luis.mhaske007@outlook.com>
2023-09-25 00:30:59 +0100

    account balance issue fixed working on list table creation

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ end ->
 
 """
 COMMAND_LIST = """List of commands
-  - Balance
-  - Sale
-  - Purchase
-  - Account
-  - List
-  - Warehouse
-  - Review
-  - End
+  - Balance - To add or deduct current account balance
+  - Sale - To make a sale of product
+  - Purchase - To purchase a new product
+  - Account - Shows the current balance
+  - List - Shows the total inventory
+  - Warehouse - search for a specific product to check availability.
+  - Review - History
+  - End - to exit.
 """
 balance = 0
 shelve = {}
@@ -36,8 +36,8 @@ while True:
 
     elif action == "balance":
         add_sub_list = """Select an option
-        - add
-        - subtract
+        - Add
+        - Subtract
         - Type 'Stop' to return to main menu."""
         print(add_sub_list)
         option = input(": ")
@@ -58,25 +58,41 @@ while True:
                 print(f"{option} is incorrect, please enter a correct input")
 
     elif action == "sale":
-        print("sale")
+        product_name = input("Please enter the name of the product: ")
+        product_quantity = int(input("Please enter the quantity:  "))
+        if product_name not in shelve or shelve[product_name]["quantity"] < 1:
+            print(f"Transaction Error!! Product {product_name} is out of stock in our warehouse.")
+            continue
+
+        shelve[product_name]["quantity"] -= product_quantity
+        balance += shelve[product_name]["price"] * product_quantity
+        print(shelve)
 
     elif action == "purchase":
-        product_name = input("Please enter the name of the product purchased: ")
-        product_price = int(input("Please enter the price of the product: "))
-
+        product_name = input("Please enter the name of the product to purchase: ")
+        product_price = float(input("Please enter the current price per piece: "))
+        product_quantity = int(input("Please enter the quantity:  "))
+        total_price = product_price*product_quantity
+        print(total_price)
         if product_name not in shelve:
             shelve[product_name] = {"price": 0, "quantity": 0}
 
-        shelve[product_name]["quantity"] += 1
-        shelve[product_name]["price"] = product_price
+        if balance >= total_price:
+            shelve[product_name]["quantity"] += product_quantity
+            shelve[product_name]["price"] = product_price
 
-        balance -= shelve[product_name]["price"]
+            balance -= total_price
+            print(shelve)
+        else:
+            print("Unable to purchase, please check account balance")
 
     elif action == "account":
-        print(f"Current Balance is: {balance}")
+        print(f"Current Account Balance is: {balance}")
 
     elif action == "list":
-        print("list")
+        print("Product name | Pricing | Quantity")
+        for product, product_stats in shelve.items():
+            print(f"{product:12.12s} | {product_stats['price']:7} | {product_stats['quantity']:7}")
 
     elif action == "warehouse":
         print("warehouse")
@@ -86,3 +102,4 @@ while True:
 
     else:
         print(f"{action} is incorrect, please enter a correct input")
+        continue
