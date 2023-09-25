8e5fc98fa63b25aae1c964e84f964e4bbfdecdb5
Luis Mhaske <luis.mhaske007@outlook.com>
2023-09-25 22:23:49 +0100

    Completed program test completed.

--- main.py
+++ main.py
@@ -25,10 +25,15 @@ COMMAND_LIST = """List of commands
 """
 balance = 0
 shelve = {}
+history = []
 while True:
     print(COMMAND_LIST)
-    action = input("Select an option from above: ")
+    # try:
+    action = input("Select an option from above list of commands: ")
+    # except EOFError:
+    #     print("Reached the end")
     action = action.casefold()
+    history.append(f"Task performed {action}")
 
     if action == "end":
         print("Ending the instance")
@@ -49,10 +54,12 @@ while True:
             elif option == "add":
                 addition = int(input("How much amount do you want to add?:"))
                 balance += addition
+                history.append(f"Added {addition} amount  to the current balance")
                 break
             elif option == "subtract":
                 sub = int(input("How much amount do you want to deduct?:"))
                 balance -= sub
+                history.append(f"Remove {sub} amount to the current balance")
                 break
             else:
                 print(f"{option} is incorrect, please enter a correct input")
@@ -66,23 +73,25 @@ while True:
 
         shelve[product_name]["quantity"] -= product_quantity
         balance += shelve[product_name]["price"] * product_quantity
-        print(shelve)
+        print(f"{product_quantity} {product_name} have been sold.")
+        history.append(f"Sold Product detail: Name: {product_name} | Quantity: {product_quantity} ")
 
     elif action == "purchase":
         product_name = input("Please enter the name of the product to purchase: ")
         product_price = float(input("Please enter the current price per piece: "))
         product_quantity = int(input("Please enter the quantity:  "))
-        total_price = product_price*product_quantity
+        total_price = product_price * product_quantity
         print(total_price)
         if product_name not in shelve:
-            shelve[product_name] = {"price": 0, "quantity": 0}
+            shelve[product_name] = {"price": 0.0, "quantity": 0}
 
         if balance >= total_price:
             shelve[product_name]["quantity"] += product_quantity
             shelve[product_name]["price"] = product_price
-
             balance -= total_price
-            print(shelve)
+            print(f"{product_name} has been added to the shelve.")
+            history.append(f"Product added to shelve: Name: {product_name} | Price: {product_price} | "
+                           f"Quantity: {product_quantity} ")
         else:
             print("Unable to purchase, please check account balance")
 
@@ -95,10 +104,34 @@ while True:
             print(f"{product:12.12s} | {product_stats['price']:7} | {product_stats['quantity']:7}")
 
     elif action == "warehouse":
-        print("warehouse")
+        product_name = input("Please enter the name of product to check availability: ")
+        if product_name in shelve and shelve[product_name]["quantity"] > 0:
+            print(f"{product_name} is available in the warehouse ")
+        else:
+            print(f"{product_name} is out of stock ")
+        history.append(f"Searched for {product_name}.")
 
     elif action == "review":
-        print("review")
+        print("Review recorded operations:")
+        from_idx = input("Enter 'from' index (or press Enter for the beginning): ")
+        to_idx = input("Enter 'to' index (or press Enter for the end): ")
+        try:
+            from_idx = int(from_idx) \
+                if from_idx \
+                else 0
+            to_idx = int(to_idx) \
+                if to_idx \
+                else len(history)
+        except ValueError:
+            print("Invalid indices. Please enter valid numbers.")
+            continue
+        # Check if the indices are out of range
+        if from_idx < 0 or from_idx >= len(history) or to_idx < 0 or to_idx > len(history) or from_idx > to_idx:
+            print("Invalid indices. Please enter valid indices within the range.")
+            continue
+        # Display recorded operations within the specified range
+        for idx in range(from_idx, to_idx):
+            print(f"{idx + 1}: {history[idx]}")
 
     else:
         print(f"{action} is incorrect, please enter a correct input")
